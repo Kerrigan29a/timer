e9dd716823aad8c0d244183a4fc7d36e23cd3eec
Javier Escalada <kerrigan29a@gmail.com>
2018-05-30 18:58:44 +0200

    Minor change in release script

--- new_release.sh
+++ new_release.sh
@@ -8,23 +8,37 @@ TO_UPDATE=(
 
 echo -n "Current version is $CURRENT_VERSION, select new version: "
 read NEW_VERSION
+echo "********************************************************************************"
 echo "Creating version $NEW_VERSION"
+echo "********************************************************************************"
 
-echo "    Starting release $NEW_VERSION"
+echo "********************************************************************************"
+echo "Starting release $NEW_VERSION"
+echo "********************************************************************************"
 git flow release start $NEW_VERSION
 
 for file in "${TO_UPDATE[@]}"
 do
-    echo "    Patching $file"
+    echo "********************************************************************************"
+    echo "Patching $file"
+    echo "********************************************************************************"
     sed -i "s/$CURRENT_VERSION/$NEW_VERSION/g" $file
     git add $file
 done
 
+echo "********************************************************************************"
 git commit -m "Releasing $NEW_VERSION"
-echo "    Finishing release $NEW_VERSION"
+echo "********************************************************************************"
+echo "********************************************************************************"
+echo "Finishing release $NEW_VERSION"
+echo "********************************************************************************"
 git flow release finish $NEW_VERSION
 
-echo "    Pushing release $NEW_VERSION"
+echo "********************************************************************************"
+echo "Pushing release $NEW_VERSION"
+echo "********************************************************************************"
 git push origin $NEW_VERSION
 
+echo "********************************************************************************"
 echo "DONE"
+echo "********************************************************************************"
